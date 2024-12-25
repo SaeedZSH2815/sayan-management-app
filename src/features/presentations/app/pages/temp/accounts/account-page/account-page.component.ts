e149f3acb755db05cc96a183a7b49f76d59a84ee
KING22\Administrator <saeedzsh2815@gmail.com>
2024-12-26 00:53:45 +0330

    edit

--- src/features/presentations/app/pages/temp/accounts/account-page/account-page.component.ts
+++ src/features/presentations/app/pages/temp/accounts/account-page/account-page.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
-import { AddCcountComponent } from '../add-ccount/add-ccount.component';
 import { AccountComponent } from '../account/account.component';
 import { CommonModule } from '@angular/common';
 import { AccountService } from '../../../../../../../core/services/account-service/account.service';
 import { AccountInterface } from '../../../../../../../core/interfaces-app/account-interface';
+import { AddAccountComponent } from '../add-ccount/add-account.component';
 
 @Component({
   selector: 'app-account-page',
-  imports: [CommonModule,AddCcountComponent,AccountComponent],
+  imports: [CommonModule,AddAccountComponent,AccountComponent],
   templateUrl: './account-page.component.html',
   styleUrl: './account-page.component.scss'
 })
 export class AccountPageComponent {
   accountList? : AccountInterface[];
-   
+
 
   constructor(private accountService : AccountService){
     this.accountList = this.accountService.accountList;
