4f6d21dc6d054b993eec84f92ac07804356ecd9c
MMSSAA <m.soheili71@gmail.com>
2025-01-02 00:07:15 +0330

    forms_1

--- src/features/presentations/app/pages/temp/page-forms/user-form/user-form.component.ts
+++ src/features/presentations/app/pages/temp/page-forms/user-form/user-form.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormsModule, NgForm } from '@angular/forms';
 import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-user-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.scss'
 })
 export class UserFormComponent implements OnInit {
   title = 'web-app';
 
+  //@ViewChild('f') FormContro
+
   ngOnInit(): void {
     initFlowbite();
+  }
+
+  onSubmit(f : any){
+    console.log(f);
   }
 
 }
