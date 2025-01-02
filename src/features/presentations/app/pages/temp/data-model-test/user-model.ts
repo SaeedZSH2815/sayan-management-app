577d101b7265354424e00630f471fbe1f66f6499
KING22\Administrator <saeedzsh2815@gmail.com>
2025-01-02 14:08:04 +0330

    Form And FormGroup And ngModel

---
+++ src/features/presentations/app/pages/temp/data-model-test/user-model.ts
@@ -0,0 +1,15 @@
+export interface IUserModel
+{
+  userName : string;
+  userCode : number;
+
+  cityName   : string;
+  userCityName   : string;
+  userStreetName : string;
+}
+
+export interface IUserAddress
+{
+  cityName   : string;
+  streetName : string;
+}
--- src/features/presentations/app/pages/temp/page-forms/user-form/user-form.component.ts
+++ src/features/presentations/app/pages/temp/page-forms/user-form/user-form.component.ts
@@ -1,16 +1,21 @@
+import { JsonPipe } from '@angular/common';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormsModule, NgForm } from '@angular/forms';
 import { initFlowbite } from 'flowbite';
+import { IUser } from '../../users/user-detail/user-detail-resolve';
+import { IUserModel } from '../../data-model-test/user-model';
+
+
 
 @Component({
   selector: 'app-user-form',
-  imports: [FormsModule],
+  imports: [FormsModule,JsonPipe],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.scss'
 })
 export class UserFormComponent implements OnInit {
   title = 'web-app';
-
+  public userModel : IUserModel = {userCityName:"",userCode:0,userName:"",userStreetName:"",cityName:""};
   //@ViewChild('f') FormContro
 
   ngOnInit(): void {
