985262b763edeed106bf6faf937c639998820dae
MMSSAA <m.soheili71@gmail.com>
2025-01-06 20:16:20 +0330

    Custom Asyn --Validate

--- src/features/presentations/app/pages/temp/page-forms/shopping-form/shopping-form.component.ts
+++ src/features/presentations/app/pages/temp/page-forms/shopping-form/shopping-form.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-form',
-  imports: [],
+  imports: [FormsModule,ReactiveFormsModule,CommonModule],
   templateUrl: './shopping-form.component.html',
   styleUrl: './shopping-form.component.scss'
 })
-export class ShoppingFormComponent {
+export class ShoppingFormComponent implements OnInit,AfterViewInit {
+  ngAfterViewInit(): void {
+  
+  }
+  @ViewChild("shoppingForm") shoppingForm! : NgForm;
+  
+  ngOnInit(): void {
+    let x = "asd";
+    
+  }
+
+  amountInputPattern = "/[^0-9]/g";
+  
+  addSubmitShopingForm(clShoppingForm : NgForm){
+    if(!this.shoppingForm.controls["productName"].valid)
+      alert("Product Name Invalid");
+    //if(clShoppingForm.getContr
+    console.log(this.shoppingForm.controls);
+  }
 
 }
