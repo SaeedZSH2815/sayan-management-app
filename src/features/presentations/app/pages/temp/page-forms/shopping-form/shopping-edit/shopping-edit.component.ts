188cd1bad8523b283babab5e2c60b0128780b1a5
KING22\Administrator <saeedzsh2815@gmail.com>
2025-01-07 16:34:35 +0330

    json

---
+++ src/features/presentations/app/pages/temp/page-forms/shopping-form/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,80 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
+import { ArrayFilterPipe } from '../../../../../../../../core/pipes/array-filter.pipe';
+import { ShortLengthPipe } from '../../../../../../../../core/pipes/short-length.pipe';
+import { ShoppingListService } from '../shopping-list.service';
+import { Subscription } from 'rxjs';
+import { Ingredient } from '../shared/ingredient.model';
+import * as data from "../../../../../../../../../public/assets/adm.json"
+
+@Component({
+  selector: 'app-shopping-edit',
+  imports: [FormsModule,ReactiveFormsModule,CommonModule,ShortLengthPipe,ArrayFilterPipe],
+  templateUrl: './shopping-edit.component.html',
+  styleUrl: './shopping-edit.component.scss'
+})
+export class ShoppingEditComponent implements OnInit,OnDestroy {
+    @ViewChild("shoppingForm") shoppingForm! : NgForm;
+
+    projectNameCaption = "Project Name Caption";
+    amountInputPattern = "^[1-9]+[0-9]*$";
+    subscription? : Subscription;
+    shoppingList : {productName:string,productId:number}[] =
+    [
+      {productId:1,productName:"bread"}
+    ];
+
+    editMode : boolean = false;
+    editItemIndex?:number;
+    editItem? : Ingredient;
+   constructor(private slService:ShoppingListService){
+
+   }
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    console.log( data);
+    this.subscription = this.slService.startedEditing.subscribe(
+      (res:number)=>{
+        //console.log("edit",res);
+        this.editMode = true;
+        this.editItemIndex = res;
+        this.editItem = this.slService.getIngredient(res);
+        this.shoppingForm.form.patchValue({"productName":this.editItem.name});
+        this.shoppingForm.form.patchValue({"amount":this.editItem.amount});
+      }
+    );
+  }
+
+
+
+    addSubmitShopingForm(clShoppingForm : NgForm){
+      //this.shoppingList.push({productId:2,productName:"bread1"});
+      //if(!this.shoppingForm.controls["productName"].valid)
+      //  alert("Product Name Invalid");
+
+      const frmValues = clShoppingForm.value;
+      console.log(frmValues);
+      const newItem = new Ingredient(frmValues["productName"],frmValues["amount"]);
+      if(this.editMode)
+      {
+        this.slService.updateIngredient(this.editItemIndex!,newItem);
+      }
+      else
+      this.slService.addIngredient(newItem);
+      clShoppingForm.reset();
+    }
+
+
+
+
+
+
+
+
+}
+
+
--- src/features/presentations/app/pages/temp/page-forms/shopping-form/shopping-form.component.ts
+++ src/features/presentations/app/pages/temp/page-forms/shopping-form/shopping-form.component.ts
@@ -1,42 +1,48 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { FormGroup, FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
-import { ShortLengthPipe } from '../../../../../../../core/pipes/short-length.pipe';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+
 import { ArrayFilterPipe } from '../../../../../../../core/pipes/array-filter.pipe';
+import { ShoppingListService } from './shopping-list.service';
+import { Ingredient } from './shared/ingredient.model';
+import { Subscription } from 'rxjs';
+import { ThisReceiver } from '@angular/compiler';
+import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
 
 @Component({
   selector: 'app-shopping-form',
-  imports: [FormsModule,ReactiveFormsModule,CommonModule,ShortLengthPipe,ArrayFilterPipe],
+  imports: [CommonModule,ArrayFilterPipe,ShoppingEditComponent],
   templateUrl: './shopping-form.component.html',
-  styleUrl: './shopping-form.component.scss'
+  styleUrl: './shopping-form.component.scss',
+  providers:[ShoppingListService]
 })
-export class ShoppingFormComponent implements OnInit,AfterViewInit {
+export class ShoppingFormComponent implements OnInit,AfterViewInit,OnDestroy {
+
+  ingredients: Ingredient[] = [];
+  private subscription?: Subscription;
+  constructor(private slService : ShoppingListService){
+
+  }
+  ngOnDestroy(): void {
+    if(this.subscription)
+      this.subscription.unsubscribe();
+  }
+
+
   ngAfterViewInit(): void {
-  
+
   }
-  @ViewChild("shoppingForm") shoppingForm! : NgForm;
-  
   ngOnInit(): void {
-    let x = "asd";
-    for (let index = 0; index < 10000; index++) {
-      this.shoppingList.push({productId:index+1,productName:"bread"+index});
-      
-    }
+   this.ingredients = this.slService.getIngredients();
+   this.subscription = this.slService.ingredientsChanged.subscribe(
+    (res:Ingredient[])=> this.ingredients = res
+   );
   }
 
-  projectNameCaption = "Project Name Caption";
-  amountInputPattern = "/[^0-9]/g";
-  shoppingList : {productName:string,productId:number}[] = 
-  [
-    {productId:1,productName:"bread"}
-  ];
-
-  addSubmitShopingForm(clShoppingForm : NgForm){
-    this.shoppingList.push({productId:2,productName:"bread1"});
-    if(!this.shoppingForm.controls["productName"].valid)
-      alert("Product Name Invalid");
-    //if(clShoppingForm.getContr
-    console.log(this.shoppingForm.controls);
+  onEdit(clIndex : number){
+   this.slService.startedEditing.next(clIndex);
   }
+
+
+
 
 }
